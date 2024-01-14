d2921b0a4cecfd549c32b805cf30d6ffb235be26
chaelin kim <gjf0055@gmail.com>
2024-01-15 00:13:00 +0900

    nuxt3 섹션2

---
+++ nuxt3-start/layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <NuxtLink to="/">index</NuxtLink>
+    <NuxtLink to="/main">main</NuxtLink>
+    <div>해더 {{ $route.name }}</div>
+    <slot />
+  </div>
+</template>
---
+++ nuxt3-start/pages/main.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>메인</h1>
+    <ProductList></ProductList>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
